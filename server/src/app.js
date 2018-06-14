1d09d4c46e32e1ad6e1325cd96b6988429f61907
EricR <rasliche@gmail.com>
2018-06-14 18:54:20 -0400

    Basic server concepts

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
 console.log("hello");
 console.log("Bro!");
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello World'
+    });
+})
+
+app.listen(process.env.PORT || 8081);
