c5e6e81fe5df77780f444c1981c381fdfa91ec83
EricR <rasliche@gmail.com>
2018-06-13 21:17:22 -0400

    Affiliate page created and routed

--- src/router.js
+++ src/router.js
@@ -2,15 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Affiliates from './views/Affiliates.vue'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+        path: '/affiliates',
+        name: 'affiliates',
+        component: Affiliates
     },
     {
       path: '/about',
---
+++ src/views/Affiliates.vue
@@ -0,0 +1,9 @@
+<template lang="html">
+<div>
+    <h1>Blue Star Affiliates</h1>
+    <p>The Blue Star affiliate program is a way we can all work together to grow our Florida Keys ecosystem steward family and share the benefits. Thanks to some awesome partners you can get some seriously cool swag at a discount, all while helping to support the Blue Star programs.</p>
+</div>
+</template>
+
+<style lang="css">
+</style>
