2b9e92b2642e20058f0abfc78db40859efe1f73e
EricR <rasliche@gmail.com>
2018-06-14 18:05:43 -0400

    Add Affiliates view

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav" class="bg-blue">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/affiliates">Affiliates</router-link>
     </div>
     <router-view/>
   </div>
